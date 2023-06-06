a8d69aaa2dbf9cac60fb0ae0c26041199ed36f68
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-06 11:33:49 -0300

    Upload de arquivos no AdminJs

---
+++ src/adminJs/resources/index.ts
@@ -0,0 +1,23 @@
+// src/adminjs/resources.ts
+
+import { ResourceWithOptions } from "adminjs";
+import { Category, Course, Episode } from "../../models";
+import { categoryResourceOptions } from "./category";
+import { courseResourceOptions } from "./course";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+
+export const adminJsResources: ResourceWithOptions[] = [
+  {
+    resource: Course,
+    options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
+  },
+  {
+    resource: Category,
+    options: categoryResourceOptions,
+  },
+]
