4a523aa2f425c72e5c9a1ab8cc7ee24da5c7451d
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-08 16:13:22 -0300

    Buscando por cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { courseService } from "../services/courseService"
+import { getPaginationParams } from "../helpers/getPaginationParams"
 
 export const coursesController = {
 
@@ -38,7 +39,22 @@ export const coursesController = {
                 return res.status(400).json({message: err.message})
             }
         }
-    }
+    },
+
+    search: async (req: Request, res: Response) => {
+            const { name } = req.query
+            const [ page, perPage] = getPaginationParams(req.query)
+        try {
+            if(typeof name !== 'string') throw new Error ('name param must be of type string')
+
+           const courses = await courseService.findByName(name, page, perPage)
+           return res.json(courses)
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
 
 
 }
