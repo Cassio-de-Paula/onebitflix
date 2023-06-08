921535dd6cb49854a141cd49afcb65748b59c3f6
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-08 15:42:15 -0300

    Obtendo 3 cursos em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import { courseService } from "../services/courseService"
 
 export const coursesController = {
+
     show: async (req: Request, res: Response) => {
         const { id } =  req.params
 
@@ -13,5 +14,19 @@ export const coursesController = {
                 return res.status(400).json({message: err.message})
             }
         }
-    }
+    },
+
+    featured: async (req: Request, res: Response) => {
+
+        try {
+           const featuredCourses = await courseService.getRandomFeaturedCourses()
+           return res.json(featuredCourses)
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+
 }
