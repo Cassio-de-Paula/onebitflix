4e7dc9dd70a76770c3994ed37d7ed4c311b94e69
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-10 20:41:59 -0300

    Criação do header (home no auth

--- onebitflix-front-end/pages/index.tsx
+++ onebitflix-front-end/pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from "next/head";
-import styles from '../styles/homeNoAuth.modules.scss'
+import styles from '../styles/homeNoAuth.module.scss'
+import HeaderNoAuth from "@/components/services/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = function () {
   return (
 		<>
 			<Head>
-                <title></title>
-            </Head>
-			<main></main>
+         <title>Onebitflix</title>
+         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+         <meta property="og:title" content="Onebitflix" key="title"/>
+         <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de forma simples e fácil!"/>
+      </Head>
+			<main><HeaderNoAuth/></main>
 		</>
   )
 };
