2f358e051fbd1102b851aa3376e1f0a277f079d8
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-10 18:19:15 -0300

    Configuração inicial do projeto

---
+++ onebitflix-front-end/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from '../styles/homeNoAuth.modules.scss'
+
+const HomeNotAuth = function () {
+  return (
+		<>
+			<Head>
+                <title></title>
+            </Head>
+			<main></main>
+		</>
+  )
+};
+
+export default HomeNotAuth;
