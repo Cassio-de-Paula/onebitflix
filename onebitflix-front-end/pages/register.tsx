28b159b39555b76a892f0310e24cc0267e5eb4c8
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-11 13:29:56 -0300

    Conexão do registro com o back end

--- onebitflix-front-end/pages/register.tsx
+++ onebitflix-front-end/pages/register.tsx
@@ -3,8 +3,53 @@ import styles from '../styles/resgisterLogin.module.scss'
 import Head from 'next/head'
 import { Form, FormGroup, Label, Container, Input, Button} from 'reactstrap'
 import Footer from '../src/components/common/footer'
+import { FormEvent, useState } from 'react'
+import authService from '@/services/authService'
+import { useRouter } from 'next/router'
+import ToastComponent from '@/components/common/toast'
+
 
 const Register = function () {
+    const router = useRouter()
+    const [toastIsOpen, setToastIsOpen] = useState(false)
+    const [toastMessage, setToastMessage] = useState('')
+    const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget)
+        const firstName = formData.get('firstName')!.toString()
+        const lastName = formData.get('lastName')!.toString()
+        const phone = formData.get('phone')!.toString()
+        const birth = formData.get('birth')!.toString()
+        const email = formData.get('email')!.toString()
+        const password = formData.get('password')!.toString()
+        const confirmPassword = formData.get('confirmPassword')!.toString()
+
+        const params = { firstName, lastName, phone, birth, email, password }
+
+        if(password !== confirmPassword) {
+            setToastIsOpen(true)
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 1000 * 3)
+            setToastMessage('Senha e confirmação diferentes')
+
+            return
+        }
+
+        const {data, status} = await authService.register(params)
+
+        if(status === 201) {
+            router.push('/login?registered=true')
+        } else {
+            setToastIsOpen(true)
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 1000 * 3)
+            setToastMessage(data.message)
+        }
+    }
+
     return (
 
         <>
@@ -17,7 +62,7 @@ const Register = function () {
             <HeaderGeneric logoUrl='/' btnUrl='/logion' btnContent='Quero fazer login'/>
         <Container className='py-5'>
             <p className={styles.formTitle}><strong>Bem Vindo(a) ao Onebitflix!</strong></p>
-            <Form className={styles.form}>
+            <Form className={styles.form} onSubmit={handleRegister}>
                 <p className='text-center'><strong>Faça sua conta!</strong></p>
                 <FormGroup>
                     <Label for='firstName' className={styles.label}>NOME</Label>
@@ -41,19 +86,19 @@ const Register = function () {
                 </FormGroup>
                 <FormGroup>
                     <Label for='password' className={styles.label}>SENHA</Label>
-                    <Input id='password' name='password' type='password' placeholder='Digite o sua senha' required className={styles.input}/>
+                    <Input id='password' name='password' type='password' placeholder='Digite a sua senha' required className={styles.input}/>
                 </FormGroup>
                 <FormGroup>
-                    <Label for='password' className={styles.label}>CONFIRME SUA SENHA</Label>
-                    <Input id='password' name='password' type='password' placeholder='Confirme a sua senha' required className={styles.input}/>
+                    <Label for='confirmPassword' className={styles.label}>CONFIRME SUA SENHA</Label>
+                    <Input id='confirmPassword' name='confirmPassword' type='password' placeholder='Confirme a sua senha' required className={styles.input}/>
                 </FormGroup>
                 <Button type='submit' outline className={styles.formBtn}>
                     CADASTRAR
                 </Button>
             </Form>
         </Container>
-        <Footer></Footer>
-
+        <Footer/>
+        <ToastComponent color='bg-danger' isOpen={toastIsOpen} message={toastMessage}/>
         </main>
         </>
     )
