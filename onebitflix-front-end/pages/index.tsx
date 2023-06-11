581e9909961ffb2dc9d6d781cc429bc622212852
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-11 11:13:37 -0300

    Criação do footer

--- onebitflix-front-end/pages/index.tsx
+++ onebitflix-front-end/pages/index.tsx
@@ -7,6 +7,7 @@ import CardsSection from "../src/components/homeNoAuth/cardsSection";
 import SlideSection from "../src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "../src/services/courseService";
+import Footer from '../src/components/common/footer';
 
 interface IndexPageProps {
   children?: ReactNode
@@ -29,7 +30,7 @@ const HomeNotAuth = function ({course}: IndexPageProps) {
         </div>
         <CardsSection/>
         <SlideSection newestCourses={course}/>
-        
+        <Footer/>
       </main>
 		</>
   )
