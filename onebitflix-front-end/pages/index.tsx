f362af8009515218aec226a206202867d38476e7
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-06-10 23:36:54 -0300

    Finalização das estruturas dos slides

--- onebitflix-front-end/pages/index.tsx
+++ onebitflix-front-end/pages/index.tsx
@@ -1,10 +1,19 @@
+import { ReactNode } from 'react'
 import Head from "next/head";
 import styles from '../styles/homeNoAuth.module.scss'
 import HeaderNoAuth from '../src/components/homeNoAuth/headerNoAuth';
 import PresentationSection from "../src/components/homeNoAuth/presentationSection";
 import CardsSection from "../src/components/homeNoAuth/cardsSection";
+import SlideSection from "../src/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseService, { CourseType } from "../src/services/courseService";
 
-const HomeNotAuth = function () {
+interface IndexPageProps {
+  children?: ReactNode
+  course: CourseType[]
+}
+
+const HomeNotAuth = function ({course}: IndexPageProps) {
   return (
 		<>
 			<Head>
@@ -19,10 +28,21 @@ const HomeNotAuth = function () {
           <PresentationSection/> 
         </div>
         <CardsSection/>
+        <SlideSection newestCourses={course}/>
         
       </main>
 		</>
   )
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses()
+  return {
+    props: {
+      course: res.data,
+    },
+    revalidate: 3600 * 24
+  }
+}
 
 export default HomeNotAuth;
